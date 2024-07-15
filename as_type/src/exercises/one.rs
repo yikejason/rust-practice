a513e525c87c9ec1405a540814bfa12786d12462
yikejason <18215513386@163.com>
2024-07-15 09:26:52 +0800

    feature: as_type practice

---
+++ as_type/src/exercises/one.rs
@@ -0,0 +1,14 @@
+// 修复错误，填空
+// 不要移除任何代码
+pub(crate) fn one() {
+    // let decimal = 97.123_f32;
+
+    // let integer: u8 = decimal as u8;
+
+    // let _c1: char = decimal as u8 as char;
+    // let _c2 = integer as char;
+
+    // assert_eq!(integer, 'b' as u8 - 1);
+
+    println!("Success!")
+}
---
+++ as_type/src/main.rs
@@ -0,0 +1,6 @@
+use as_type::{execute, ExercisesType};
+
+fn main() {
+    // a few exercise about this one
+    execute(ExercisesType::Four);
+}
